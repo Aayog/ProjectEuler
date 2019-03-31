d1ef3fa43e14867da02acd67093ccb076c98fc4d
Aayog Koirala <koiralaa@uindy.edu>
2019-03-31 22:42:50 +0000

    C++ only in this branch

--- Python/multiple.py
+++
@@ -1,9 +0,0 @@
-n =  1000
-sum = 0
-
-for i in range(n):
-    if i % 3 == 0  or i % 5 == 0:
-        print(i)
-        sum += i
-
-print(sum)
--- Python/square.py
+++
@@ -1,4 +0,0 @@
-from Rectangle import __Rect
-
-square = __Rect(10,10)
-print(square.width)
