c9fccd558c1ea556d9d0f9ee8d9c455496c77f37
Aayog Koirala <koiralaa@uindy.edu>
2019-03-31 22:46:54 +0000

    Removed mistakenly added Python file

--- Python/Rectangle.py
+++
@@ -1,9 +0,0 @@
-
-class __Rect:
-    def __init__(self, w, h):
-        self.width = w
-        self.height = h
-    def getW(self):
-        return self.width
-    def getH(self):
-        return self.height
