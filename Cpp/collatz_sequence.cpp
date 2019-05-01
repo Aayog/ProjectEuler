2892d069edc8dea0b88664bdd3606517aa6d45d8
Aayog Koirala <koiralaa@uindy.edu>
2019-05-01 20:14:35 +0000

    COllatz

--- Cpp/collatz_sequence.cpp
+++ Cpp/collatz_sequence.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-long lengthChain(int num){
+long lengthChain(long num){
     long count = 1;
     while(num > 1){
         if (num % 2 == 0){
-            num /= 2;
+            num = num / 2;
         }else{
             num = 3 * num + 1;
         }
@@ -16,18 +16,35 @@ long lengthChain(int num){
     return count;
 }
 
+long length = 0;
+
+long collatz(long num){
+    length++;
+    if (num == 1){
+        return length;
+    }
+    if(num % 2 == 0){
+        collatz(num / 2);
+    }else {
+        collatz(num * 3 + 1);
+    }
+    
+}
+
 int main(void){
     long x = 0;
-    const long MAX = 10000000;
+    const long MAX = 1000000;
     long high = 0;
     long ans = 0;
     while (x <= MAX){
         long len = lengthChain(x);
-        if(len > high){
+        if(len >= high){
             high = len;
             ans = x;
         }
         x++;
     }
     cout << "high : " << high << " ans: " << ans << endl;
+
+    cout << collatz(837799) << endl;
 }
